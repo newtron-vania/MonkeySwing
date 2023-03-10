2fec1abcffe2700751ec60e774ccf68f8d08921c
newtron-vania <galtab0403@gmail.com>
2023-03-10 13:40:25 +0900

    Bug fixed

---
+++ Assets/Scripts/Weight.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Weight : MonoBehaviour
+{
+    [SerializeField]
+    protected MonkeyController monkey;
+    TextMeshProUGUI weightText;
+
+    private void Start()
+    {
+        StartCoroutine("FindMonkey");
+        weightText = this.GetComponent<TextMeshProUGUI>();
+    }
+
+    void Update()
+    {
+        weightText.text = $"Weight : {monkey.weight}";
+    }
+
+    IEnumerator FindMonkey()
+    {
+        while (GameObject.FindWithTag("Monkey") == null)
+        {
+            yield return new WaitForFixedUpdate();
+        }
+        monkey = GameObject.FindWithTag("Monkey").GetComponent<MonkeyController>();
+        //Debug.Log($"{this.transform.name} find {monkey.transform.name}");
+    }
+}
