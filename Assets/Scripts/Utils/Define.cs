a65aa762f2c4624b8558c473058c7a4356088038
newtron-vania <galtab0403@gmail.com>
2023-03-17 11:35:49 +0900

    - Game Manager Update - Scene  Created(Main Scene, Game Scene, Loading Scene) - pendulum motion Update

---
+++ Assets/Scripts/Utils/Define.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Define
+{
+
+    public enum WorldObject
+    {
+        Unknown,
+        Player,
+        Monster,
+    }
+
+    public enum State
+	{
+		Die,
+		Moving,
+		Idle,
+		Skill
+    }
+public enum Layer
+    {
+        Ground = 8,
+        Block = 9,
+        Monster = 10,
+    }
+    public enum Scene
+    {
+        Unknown,
+        Login,
+        Lobby,
+        Game
+    }
+    public enum Sound
+    {
+        Bgm,
+        Effect,
+        MaxCount,
+    }
+    public enum UIEvent
+    {
+        Click,
+        Drag,
+    }
+    public enum MouseEvent
+    {
+        Press,
+        PointerDown,
+        PointerUp,
+        Click,
+    }
+
+    public enum CameraMode
+    {
+        QuarterView,
+    }
+
+}
