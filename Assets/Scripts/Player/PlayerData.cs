fc33c9207104a404bb6f1a199c92d5a680f5e714
jung_hyeon <j01h02@gmail.com>
2023-04-12 02:41:22 +0900

    shop 저장방식 수정

--- Assets/Scripts/Player/PlayerData.cs
+++ Assets/Scripts/Player/PlayerData.cs
@@ -12,7 +12,7 @@ public class PlayerData
     int currentSkinId = 0;
     public int MonkeySkinId { get { return currentSkinId; } set { currentSkinId = value; SetData(); } }
     [SerializeField]
-    List<int> collectedSkinId = new List<int>();
+    public List<int> collectedSkinId = new List<int>();
     [SerializeField]
     int bestScore = 0;
     public int BestScore { get { return bestScore; } set { bestScore = value; SetData(); UploadToBestScoreRanking(); } }
